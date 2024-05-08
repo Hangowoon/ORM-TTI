1045ed1aaf0da89d991a3ec9c4f80f24c14889b5
sungwoo <swpheus1@gmail.com>
2024-05-08 15:53:26 +0900

    feat:www.js app.js 통합 package.json start start: nodemon ./app.js 로 업데이트

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var http = require("http");
+var debug = require("debug")("tti:server");
+
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -93,5 +96,57 @@ app.use(function (err, req, res, next) {
 	res.status(err.status || 500);
 	res.render("error");
 });
+
+//서버 설정 및 시작 //순서 중요!
+var port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+//HTTP 기반 서버 생성
+var server = http.createServer(app);
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+function normalizePort(val) {
+	var port = parseInt(val, 10);
+
+	if (isNaN(port)) {
+		return val; // named pipe
+	}
+
+	if (port >= 0) {
+		return port; // port number
+	}
+
+	return false;
+}
+
+function onError(error) {
+	if (error.syscall !== "listen") {
+		throw error;
+	}
+
+	var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+	switch (error.code) {
+		case "EACCES":
+			console.error(bind + " requires elevated privileges");
+			process.exit(1);
+			break;
+		case "EADDRINUSE":
+			console.error(bind + " is already in use");
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
+}
+
+function onListening() {
+	var addr = server.address();
+	var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+	debug("Listening on " + bind);
+}
 
 module.exports = app;
